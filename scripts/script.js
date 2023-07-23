9d071bfd0a0fa0647c05c68e0984a7fe8e8d5bb5
mehran <mehransheyhki11@yahoo.com>
2023-07-24 02:00:55 +0330

    fist commit 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,32 @@
-
 // for slider menu
-let sliderSubmenu = document.getElementsByClassName("slider-submenus");
+let sliderSubmenusHandller =
+  document.getElementsByClassName("menu-has-submenu");
 let i;
 
-for (i = 0; i < sliderSubmenu.length; i++) {
-
-  sliderSubmenu[i].addEventListener("click", function() {
-    this.classList.toggle("submenu-close");
-    let panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+for (i = 0; i < sliderSubmenusHandller.length; i++) {
+  sliderSubmenusHandller[i].addEventListener("click", function () {
+    this.classList.toggle("menu-active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
-    }
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+// for slider second submenu
+let sliderSeSubmenusHandller =
+  document.getElementsByClassName("menu-has-se-submenu");
+let a;
+
+for (a = 0; a < sliderSeSubmenusHandller.length; a++) {
+  sliderSeSubmenusHandller[a].addEventListener("click", function () {
+    var panelSecond = this.nextElementSibling;
+    console.log(panelSecond);
+    if (panelSecond.style.maxHeight) {
+      panelSecond.style.maxHeight = null;
+    } else {
+      panelSecond.style.maxHeight = panelSecond.scrollHeight + "px";
+    } 
   });
 }
